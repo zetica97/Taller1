3636ace8bec09359f6bc3d59e2b0918caffdc56e
zetica97 <69682092+zetica97@users.noreply.github.com>
2020-08-21 09:30:45 -0500

    ejercicios

---
+++ ejercicio3.py
@@ -0,0 +1,14 @@
+#con FOR:
+
+#for num in range(61):
+ #   c = num*num
+    
+  #  print(f"El cuadrado de {num} es {c}")
+
+#con WHILE:
+num = 1
+while num <= 60:
+    c = num*num
+    print(f"El cuadrado de {num} es {c}")
+    num += 1
+print("fin")
